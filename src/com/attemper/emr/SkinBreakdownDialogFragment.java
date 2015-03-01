b6c80463a17ae6c86bb79a9955b6ea6801875416
Robert Acosta <robert.j.acosta@gmail.com>
2015-03-01 15:31:32 -0800

    Adds skin incision and breakdown dialog fragments and layouts

---
+++ src/com/attemper/emr/SkinBreakdownDialogFragment.java
@@ -0,0 +1,62 @@
+package com.attemper.emr;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+
+public class SkinBreakdownDialogFragment extends DialogFragment {
+	
+	/* The activity that creates an instance of this dialog fragment must
+     * implement this interface in order to receive event callbacks.
+     * Each method passes the DialogFragment in case the host needs to query it. */
+    public interface SkinBreakdownDialogListener {
+        public void onDialogPositiveClick(DialogFragment dialog);
+    }
+	
+    // Use this instance of the interface to deliver action events
+    SkinBreakdownDialogListener mListener;
+    
+	@Override
+	public Dialog onCreateDialog(Bundle savedInstanceState) {
+	    AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+	    // Get the layout inflater
+	    LayoutInflater inflater = getActivity().getLayoutInflater();
+
+	    // Inflate and set the layout for the dialog
+	    // Pass null as the parent view because its going in the dialog layout
+	    builder.setView(inflater.inflate(R.layout.fragment_skin_breakdown, null))
+	    // Add action buttons
+	           .setPositiveButton(R.string.add, new DialogInterface.OnClickListener() {
+	               @Override
+	               public void onClick(DialogInterface dialog, int id) {
+	            	   // Send the positive button event back to the host activity
+                       mListener.onDialogPositiveClick(SkinBreakdownDialogFragment.this);
+	               }
+	           })
+	           .setNegativeButton(R.string.cancel_title, new DialogInterface.OnClickListener() {
+	               public void onClick(DialogInterface dialog, int id) {
+	            	   SkinBreakdownDialogFragment.this.getDialog().cancel();
+	               }
+	           });      
+	    return builder.create();
+	}
+	
+	// Override the Fragment.onAttach() method to instantiate the NoticeDialogListener
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        // Verify that the host activity implements the callback interface
+        try {
+            // Instantiate the NoticeDialogListener so we can send events to the host
+            mListener = (SkinBreakdownDialogListener) activity;
+        } catch (ClassCastException e) {
+            // The activity doesn't implement the interface, throw exception
+            throw new ClassCastException(activity.toString()
+                    + " must implement SkinBreakdownDialogListener");
+        }
+    }
+}
